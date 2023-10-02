56f6f6fbbb897b80fda142a314c8c07215a45d01
Priya6894 <136152502+Priya6894@users.noreply.github.com>
2023-10-02 22:35:53 +0530

    Update settings.py

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -74,13 +74,16 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-  #  'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-#    }
+ DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deploy',
+        'USER': 'postgres',
+        'PASSWORD': 'priya',
+        'HOST': 'localhost',  # Set this to your PostgreSQL server's host
+        'PORT': '5432',           # Leave this empty to use the default PostgreSQL port (5432)
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
